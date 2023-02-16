fbfbd252c91f514270917cd31bcc36686ac2650f
edison <83228064+haycom1024@users.noreply.github.com>
2023-02-16 19:39:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ template_id = os.environ["TEMPLATE_ID"]
 def get_weather():
   url = "https://v0.yiketianqi.com/api?unescape=1&version=v91&appid=43656176&appsecret=I42og6Lm&ext=&cityid=&city=" + city
   res = requests.get(url).json()
-  weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp'])
+  weather = res.get('data')
+  return weather['wea'], math.floor(weather['tem2'])
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
